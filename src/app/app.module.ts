b74541e8f39e55ac74f02e0a5c17b743a5cd95e3
Jaskaranpreet Singh <jaskaranpreet.singh@ad.infosys.com>
2019-02-10 01:57:27 -0500

    minor imports change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,21 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { PokemonComponent } from './pokemon/pokemon.component';
 import { PokeService } from './pokemon/poke.service';
+import { PokeGameComponent } from './pokemon/poke-game/poke-game.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PokemonComponent
+    PokemonComponent,
+    PokeGameComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
+    AppRoutingModule,
   ],
   providers: [PokeService],
   bootstrap: [AppComponent]
