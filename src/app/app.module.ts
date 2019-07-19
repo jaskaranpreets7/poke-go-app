50b95c5f46627d8c38331a13db73092024a2d1bf
Jaskaranpreet Singh <jaskaranpreet.singh@ad.infosys.com>
2019-07-19 17:17:11 -0400

    imported playground component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,20 @@ import { PokeService } from './pokemon/poke.service';
 import { PokeGameComponent } from './pokemon/poke-game/poke-game.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
+import { PlaygroundComponent } from './pokemon/poke-game/playground/playground.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PokemonComponent,
     PokeGameComponent,
-    NavbarComponent
+    NavbarComponent,
+    PlaygroundComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    AppRoutingModule,
+    AppRoutingModule
   ],
   providers: [PokeService],
   bootstrap: [AppComponent]
