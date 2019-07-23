9a2756c2955827855eb479697d2d00fa1af03f23
Jaskaranpreet Singh <jaskaranpreet.singh@ad.infosys.com>
2019-07-23 18:08:23 -0400

    added pagination/scroll functionality

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -8,15 +8,42 @@ import { PokeService } from './poke.service';
   styleUrls: ['./pokemon.component.css']
 })
 export class PokemonComponent implements OnInit {
-  poke: any;
+  public poke: any;
+  public maxPages : any= {};
+  public filterList : any;
 
   constructor( private pokeservice: PokeService) { }
 
   ngOnInit() {
     this.poke = this.pokeservice.getData();
+
+    this.filterList = this.poke.filter((a)=>{
+      return (a.id >= 1 && a.id <= 10)
+    })
+
+    let len = Math.ceil(this.poke.length/10);
+    for(let i = 1; i<=len; i++){
+      if(this.maxPages[i]){
+        this.maxPages[i]++;
+      }else{
+        this.maxPages[i]=1;
+      }
+    }
+    this.maxPages = Object.keys(this.maxPages)
   }
 
+  pokeList(value):void{
+    let rangeFrom = value * 10 - 10 ;
+    let rangeTo = value * 10 ;
+    this.filterList = this.poke.filter((a)=>{
+      return (a.id > rangeFrom && a.id <= rangeTo)
+    })
 
+    this.topFunction()
+  }
 
-
+  topFunction():void{
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
 }
