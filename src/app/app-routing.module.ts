2e9e772b9996af9bd4556c93b4516f51303f68d9
Jaskaranpreet Singh <jaskaranpreet.singh@ad.infosys.com>
2019-07-24 17:12:17 -0400

    removed extra import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Routes,redirectTo } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import {PokemonComponent} from './pokemon/pokemon.component';
 import { PokeGameComponent } from './pokemon/poke-game/poke-game.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo:'poke', pathMatch:'full'}
+  { path: '', redirectTo:'poke', pathMatch:'full'},
   { path: 'poke', component: PokemonComponent},
   { path: 'poke-game', component: PokeGameComponent}
 ];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes),
