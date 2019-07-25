5700032c3f8de25ca3175800f9647f25f5cff529
Jaskaranpreet Singh <jaskaranpreet.singh@ad.infosys.com>
2019-07-25 17:38:51 -0400

    added media queries

--- src/app/pokemon/poke-game/playground/playground.component.ts
+++ src/app/pokemon/poke-game/playground/playground.component.ts
@@ -1,14 +1,39 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-playground',
   templateUrl: './playground.component.html',
-  styleUrls: ['./playground.component.css']
+  styleUrls: ['./playground.component.css'],
+  // animations:[
+  //   trigger('imgShake', [
+  //     state('normal' , style({
+  //       'transform':'scale(1)',
+  //     })),
+  //     state('shakeend', style({
+  //       transform: 'scale(1)',
+  //       backgroundColor:'red'
+  //     })),
+  //     transition('* <=> shakeend', animate('6000s ease-out', keyframes([
+  //       style({transform: 'translate3d(-1px, 0, 0)', offset: 0.1}),
+  //       style({transform: 'translate3d(2px, 0, 0)', offset: 0.2}),
+  //       style({transform: 'translate3d(-4px, 0, 0)', offset: 0.3}),
+  //       style({transform: 'translate3d(4px, 0, 0)', offset: 0.4}),
+  //       style({transform: 'translate3d(-4px, 0, 0)', offset: 0.5}),
+  //       style({transform: 'translate3d(4px, 0, 0)', offset: 0.6}),
+  //       style({transform: 'translate3d(-4px, 0, 0)', offset: 0.7}),
+  //       style({transform: 'translate3d(2px, 0, 0)', offset: 0.8}),
+  //       style({transform: 'translate3d(-1px, 0, 0)', offset: 0.9}),
+  //     ])))
+  //   ])
+  // ]
 })
 export class PlaygroundComponent implements OnInit {
+
   @Input() yourPoke: any;
   @Input() compPoke: any;
 
+  public state: string = 'normal';
   public player1HP: any;
   public player2HP: any;
   public compAtk: number;
@@ -31,23 +56,30 @@ export class PlaygroundComponent implements OnInit {
     this.player2HP = (Math.floor((this.compPoke.base.HP/this.compPoke.base.HP)*100));
   }
 
-  attack1():void{
+  attack1(state:string):void{
     let attackPower = Math.floor((this.yourPoke.base.Attack/4));
     this.player2HP = this.player2HP - attackPower;
     let timeout = setTimeout(()=>{this.computer() },1000);
+
+    this.state = state;
+    this.state === 'normal' ? this.state = 'shakeend' : this.state = 'normal';
+    this.state === 'shakeend' ? this.state = 'shakeend' : this.state = 'normal';
+
 
     if(this.player2HP  < 0 ){
       this.player2HP = 0;
       setTimeout(()=>{this.youWon = true;},700);  
       clearTimeout(timeout)
     }
-    this.turn = !this.turn
+    this.turn = !this.turn;
+
   }
 
-  attack2():void{
+  attack2(state:string):void{
     let attackPower = this.yourPoke.base.Attack - Math.floor((this.yourPoke.base.Attack/4));
     this.player2HP = this.player2HP - attackPower;
     let timeout = setTimeout(()=>{this.computer()},1000);
+    this.state === 'normal' ? this.state = 'shakeend' : this.state = 'normal';
 
     if(this.player2HP  < 0 ){
       this.player2HP = 0;
